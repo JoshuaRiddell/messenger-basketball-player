6a4c41ccc5e2305ecc25f65f25939ff68e73f7d5
Joshua Riddell <joshua.riddell11@gmail.com>
2016-03-30 10:28:53 +1000

    Update Arduino code
    The Arduino code I originally posted was not up to date for some reason.

--- arduino/set_servo/set_servo.ino
+++ arduino/set_servo/set_servo.ino
@@ -1,12 +1,40 @@
+#include <Servo.h>
+
+#define SERIAL_BAUD 115200
+
+#define BASE_PIN 9 // base motor
+#define EL_PIN 10 // elbow motor
+#define LIFT_PIN 11 // lift motor
+#define STATUS_LED_PIN 13 // status led
+
+#define BUFFER_SIZE 2 // incoming buffer
+
+Servo arm[3];
+
+byte incomingBuffer[BUFFER_SIZE];
+
 void setup() {
-  Serial.begin(115200);
+  // begin serial port
+  Serial.begin(SERIAL_BAUD);
+
+  // attach servos
+  arm[0].attach(BASE_PIN);
+  arm[1].attach(EL_PIN);
+  arm[2].attach(LIFT_PIN);
+
+  arm[0].write(93);
+  arm[1].write(80);
+  arm[2].write(80);
+
+  pinMode(STATUS_LED_PIN, OUTPUT);
+  digitalWrite(STATUS_LED_PIN, LOW);
 }
+
 void loop() {
-  if(Serial.available() > 0) {
-    char data = Serial.read();
-    char str[2];
-    str[0] = data;
-    str[1] = '\0';
-    Serial.print(str);
+  if (Serial.available() > 0) {
+    Serial.readBytes(incomingBuffer, BUFFER_SIZE);
+    digitalWrite(STATUS_LED_PIN, HIGH);
+    arm[incomingBuffer[0]].write(incomingBuffer[1]);
   }
+  digitalWrite(STATUS_LED_PIN, LOW);
 }
